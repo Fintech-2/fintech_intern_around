142be8f430ca0537fb98136f067142c13bd47715
eastmeet <ys932184@gmail.com>
2023-05-31 14:45:42 +0900

    first commit

---
+++ server/src/main/java/com/project/fintech/ServerApplication.java
@@ -0,0 +1,15 @@
+package com.project.fintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("classpath:env.yml")
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
